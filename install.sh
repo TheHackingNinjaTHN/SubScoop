27ab2814372b404dabd9e5e2cc32f9d253142e7d
SnipeNik <125726623+snipenik@users.noreply.github.com>
2024-01-28 16:22:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 # Check if GOPATH is set
+: <<'COMMENT'
 if [[ -z "$GOPATH" ]]; then
     echo "GOPATH is not set. Installing Go and setting environment variables..."
     wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
@@ -14,26 +20,22 @@ if [[ -z "$GOPATH" ]]; then
     echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile
     source ~/.bash_profile
 fi
+COMMENT
 
 # Install Python and other dependencies
 sudo apt-get install -y python-dnspython python-pip python3-pip python-setuptools ruby-full build-essential libssl-dev libffi-dev python-dev
 
-# Check and install missing tools
-
-# Function to check if a command exists
-check_command_exists() {
-    command -v "$1" >/dev/null 2>&1
-}
-
 # Function to install a command if it doesn't exist
 install_if_missing() {
-    if ! check_command_exists "$1"; then
-        echo "Installing $1..."
+    if ! command -v "$1" &>/dev/null; then
+        echo -e "${RED}Installing $1...${NC}"
         sudo apt-get install -y "$1"
         if [ $? -ne 0 ]; then
-            echo "Failed to install $1. Please install it manually."
+            echo -e "${RED}Failed to install $1. Please install it manually.${NC}"
             exit 1
         fi
+    else
+        echo -e "${GREEN}$1 is installed.${NC}"
     fi
 }
 
@@ -46,41 +48,25 @@ for tool in "${tools[@]}"; do
 done
 
 # Install Rust
-if ! check_command_exists "rustup"; then
+if ! command -v "rustup" &>/dev/null; then
     echo "Installing Rust..."
     curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
     source $HOME/.cargo/env
     export PATH="$HOME/.cargo/bin:$PATH" >> ~/.bash_profile
+else
+    echo -e "${GREEN}Rust is installed.${NC}"
 fi
 
 # Check and install Go tools
-go_tools=("subfinder" "amass" "httprobe")
+ go_tools=("subfinder" "amass" "httprobe")
+ Iterate through Go tools and install if missing
+ for tool in "${go_tools[@]}"; do
+     install_if_missing "$tool"
+ done
 
-# Iterate through Go tools and install if missing
-for tool in "${go_tools[@]}"; do
-    if ! check_command_exists "$tool"; then
-        echo "Installing $tool..."
-        GO111MODULE=on go install -v github.com/projectdiscovery/$tool/v2/cmd/$tool@latest
-        mv $GOPATH/bin/$tool /usr/bin/
-    fi
-done
+# Install Sublist3r
+# sudo apt-get -y install sublist3r
 
-# Check and install Python tools
-python_tools=("sublist3r" "SubDomainizer")
-
-# Iterate through Python tools and install if missing
-for tool in "${python_tools[@]}"; do
-    if ! check_command_exists "$tool"; then
-        echo "Installing $tool..."
-        git clone https://github.com/nsonaniya2010/$tool.git
-        cd $tool
-        pip install -r requirements.txt
-        chmod +x ${tool}.py
-        mv ${tool}.py /usr/bin/
-        cd ..
-        rm -rf $tool
-    fi
-done
 
 # Install Findomain
 if ! check_command_exists "findomain"; then
@@ -91,6 +77,31 @@ if ! check_command_exists "findomain"; then
     mv target/release/findomain /usr/bin/
     cd ..
     rm -rf Findomain
+    if [ $? -ne 0 ]; then
+        echo -e "\033[0;31mFailed to install Findomain. Please install it manually.\033[0m"
+        exit 1
+    else
+        echo -e "\033[0;32mFindomain is installed.\033[0m"/
+    fi
 fi
+
+# Install SubDomainizer
+git clone https://github.com/nsonaniya2010/SubDomainizer.git
+cd SubDomainizer
+pip3 install -r requirements.txt
+mv SubDomainizer.py /usr/bin/
+
+# Install Knockpy
+git clone https://github.com/guelfoweb/knock.git
+cd knock
+python3 setup.py install
+knockpy -v
+
+# Install Sublist3r
+git clone https://github.com/aboul3la/Sublist3r.git
+cd Sublist3r
+sudo pip install -r requirements.txt
+
+
 
 echo "Installation completed."
