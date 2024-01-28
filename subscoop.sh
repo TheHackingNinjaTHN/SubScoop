62129cf8de00513bd272269b40a7b94365715a06
SnipeNik <125726623+snipenik@users.noreply.github.com>
2024-01-28 17:14:28 +0530

    Update subscoop.sh

--- subscoop.sh
+++ subscoop.sh
@@ -10,27 +10,32 @@ while getopts ":u:h" opt; do
             
             # Perform subdomain enumeration using subfinder, amass, sublist3r, knockpy, findomain, subbrute, and subdomainizer and save output to subdomains.txt
             echo -e "\e[32mPerforming subdomain enumeration...\e[0m"
-            printf "\e[1;33m[RUNNING]\e[0m\n"
+            printf "\e[1;33m[Running Subfinder]\e[0m\n"
             subfinder -d "$domain" -all -silent > subdomains.txt || { echo -e "\e[31mError: subfinder failed.\e[0m"; }
+            printf "\e[1;33m[Running Amass]\e[0m\n \e[32m[#######################################]\e[0m \e[1;31mDone.\e[0m\n\n" | pv -qL 35
             amass enum -d "$domain" >> subdomains.txt || { echo -e "\e[31mError: amass failed.\e[0m"; }
+            printf "\e[1;33m[Running Sublist3r]\e[0m\n \e[32m[#######################################]\e[0m \e[1;31mDone.\e[0m\n\n" | pv -qL 35
             /sublist3r/sublist3r.py -d "$domain" >> subdomains.txt || { echo -e "\e[31mError: sublist3r failed.\e[0m"; }
+            printf "\e[1;33m[Running Knockpy]\e[0m\n \e[32m[#######################################]\e[0m \e[1;31mDone.\e[0m\n\n" | pv -qL 35
             knockpy "$domain" -silent >> subdomains.txt || { echo -e "\e[31mError: knockpy failed.\e[0m"; }
+            printf "\e[1;33m[Running Findomain]\e[0m\n \e[32m[#######################################]\e[0m \e[1;31mDone.\e[0m\n\n" | pv -qL 35
             findomain -t "$domain" >> subdomains.txt || { echo -e "\e[31mError: findomain failed.\e[0m"; }
+            printf "\e[1;33m[Running Subbrute]\e[0m\n \e[32m[#######################################]\e[0m \e[1;31mDone.\e[0m\n\n" | pv -qL 35
             subbrute.py "$domain" >> subdomains.txt || { echo -e "\e[31mError: subbrute failed.\e[0m"; }
+            printf "\e[1;33m[Running SubDomainizer]\e[0m\n \e[32m[#######################################]\e[0m \e[1;31mDone.\e[0m\n\n" | pv -qL 35
             subdomainizer -u http://"$domain" >> subdomains.txt || { echo -e "\e[31mError: subdomainizer failed.\e[0m"; }
-            printf "\e[32m[#######################################]\e[0m \e[1;31mDone.\e[0m\n\n" | pv -qL 35
-            echo -e "\e[32mSubdomain enumeration completed. Checking for live hosts...\e[0m"
+            printf "[Subdomain Enumeration Completed] \e[32m[#######################################]\e[0m \e[1;31mDone.\e[0m\n\n" | pv -qL 35
+            echo
             echo
             sleep 0.5
             echo
-
+            echo
             # Sorting and filtring uniq subdomains
-            printf "\e[1;33m[RUNNING]\e[0m\n"
+            printf "\e[1;33m[Filtering Unique SubDomains]\e[0m\n"
             cat subdomains.txt | sort -u | uniq > uniqsubs.txt
 
-            
             # Use httprobe to filter out live hosts from subdomains.txt
-            printf "\e[1;33m[RUNNING]\e[0m\n"
+            printf "\e[1;33m[Filtering Live SubDomains]\e[0m\n"
             cat uniqsubs.txt | httprobe > live_hosts.txt || { echo -e "\e[31mError: httprobe failed.\e[0m"; }
             printf "\e[32m[#######################################]\e[0m \e[1;31mDone.\e[0m\n\n" | pv -qL 35
             echo -e "\e[32mLive hosts identification completed. Check 'live_hosts.txt' for results.\e[0m"
